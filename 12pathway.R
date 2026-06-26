ebf922ee53b3d496c39d2c8a8cb0b3bb9709ac11
CeciliaNov <ztycycy@163.com>
2026-06-26 13:09:57 -0400

    Update 12pathway.R

--- 12pathway.R
+++ 12pathway.R
@@ -1,4 +1,4 @@
-# 加载所需的包
+
 library(clusterProfiler)
 library(org.Hs.eg.db)
 library(GOplot)
@@ -11,14 +11,14 @@ library(ggplot2)
 library(ggpubr)
 library(gridExtra)
 
-# 创建输出目录
+
 dirname <- "20240728enrichplot"
 dir.create(dirname)
 
-# 读取数据
+
 dep <- read.csv("diet_dep.csv")
 
-# 计算不同饮食中的差异蛋白质数据
+
 calculate_diff_proteins <- function(data, diet_label) {
   sel <- data[data$diet == diet_label, ]
   diff_proteins <- sel %>%
@@ -44,7 +44,7 @@ diff_proteins_hfhc <- calculate_diff_proteins(dep, "C")
 diff_proteins_LFLC <- calculate_diff_proteins(dep, "E")
 diff_proteins_normal <- calculate_diff_proteins(dep, "D")
 
-# 进行富集分析
+
 perform_enrichment <- function(genes) {
   enrich_result <- enrichGO(
     gene = genes,
@@ -63,7 +63,7 @@ enrich_hfhc <- perform_enrichment(diff_proteins_hfhc$SYMBOL)
 enrich_LFLC <- perform_enrichment(diff_proteins_LFLC$SYMBOL)
 enrich_normal <- perform_enrichment(diff_proteins_normal$SYMBOL)
 
-# 选择显著通路
+
 extract_significant_pathways <- function(enrich_result, diet_name) {
   significant_pathways <- enrich_result@result %>%
     filter(p.adjust < 0.05) %>%
@@ -77,20 +77,20 @@ significant_hfhc <- extract_significant_pathways(enrich_hfhc, "HFHC")
 significant_LFLC <- extract_significant_pathways(enrich_LFLC, "LFLC")
 significant_normal <- extract_significant_pathways(enrich_normal, "Normal")
 
-# 合并结果
+
 combined_results <- bind_rows(significant_high_glucose, significant_ketogenic, significant_hfhc, significant_LFLC, significant_normal)
 
-# 共同变化的通路
+
 common_pathways <- Reduce(intersect, list(significant_high_glucose$Description, significant_ketogenic$Description, significant_hfhc$Description, significant_LFLC$Description, significant_normal$Description))
 
-# 不同变化的通路
+
 unique_high_glucose <- setdiff(significant_high_glucose$Description, c(significant_ketogenic$Description, significant_hfhc$Description, significant_LFLC$Description, significant_normal$Description))
 unique_ketogenic <- setdiff(significant_ketogenic$Description, c(significant_high_glucose$Description, significant_hfhc$Description, significant_LFLC$Description, significant_normal$Description))
 unique_hfhc <- setdiff(significant_hfhc$Description, c(significant_high_glucose$Description, significant_ketogenic$Description, significant_LFLC$Description, significant_normal$Description))
 unique_LFLC <- setdiff(significant_LFLC$Description, c(significant_high_glucose$Description, significant_ketogenic$Description, significant_hfhc$Description, significant_normal$Description))
 unique_normal <- setdiff(significant_normal$Description, c(significant_high_glucose$Description, significant_ketogenic$Description, significant_hfhc$Description, significant_LFLC$Description))
 
-# 提取前5个共同变化的通路
+
 top_common_pathways <- combined_results %>%
   filter(Description %in% common_pathways) %>%
   arrange(pvalue) %>%
@@ -98,12 +98,12 @@ top_common_pathways <- combined_results %>%
   head(5) %>%
   pull(Description)
 
-# 提取共同变化的通路数据并按 pvalue 排序选择前5种
+
 common_results <- combined_results %>%
   filter(Description %in% top_common_pathways) %>%
   mutate(Category = "Common")
 
-# 提取不同变化的通路数据并按 pvalue 排序选择前5种
+
 unique_high_glucose_results <- combined_results %>%
   filter(Description %in% unique_high_glucose) %>%
   arrange(pvalue) %>%
@@ -134,10 +134,10 @@ unique_normal_results <- combined_results %>%
   head(5) %>%
   mutate(Category = "Unique to Normal")
 
-# 合并共同变化和不同变化的结果
+
 plot_data <- bind_rows(common_results, unique_high_glucose_results, unique_ketogenic_results, unique_hfhc_results, unique_LFLC_results, unique_normal_results)
 
-# 创建一个包含所有数据的ggplot
+
 plot <- ggplot(plot_data, aes(x = Diet, y = reorder(Description, pvalue), size = Count, color = -log10(p.adjust))) +
   geom_point() +
   scale_color_gradient(low = "blue", high = "red") +
@@ -158,10 +158,10 @@ plot <- ggplot(plot_data, aes(x = Diet, y = reorder(Description, pvalue), size =
   ) +
   facet_wrap(~Category, scales = "free_y", ncol = 1)
 
-# 保存图像
+
 ggsave(paste0(dirname,"/diet_comparison.tiff"), plot, width = 10, height = 15)
 
-# 绘制共同通路的 cnetplot
+
 common_result <- enrich_high_glucose
 common_result@result <- enrich_high_glucose@result %>%
   filter(Description %in% common_pathways) %>%
@@ -172,7 +172,7 @@ cnet_common <- cnetplot(common_result, showCategory = 5) +
   ggtitle("Common Pathways") + 
   theme_minimal()
 
-# 绘制 High-glucose 特有通路的 cnetplot
+
 unique_high_glucose_result <- enrich_high_glucose
 unique_high_glucose_result@result <- enrich_high_glucose@result %>% 
   filter(Description %in% unique_high_glucose) %>% 
@@ -183,7 +183,7 @@ cnet_high_glucose <- cnetplot(unique_high_glucose_result, showCategory = 5) +
   ggtitle("Unique to High-glucose") + 
   theme_minimal()
 
-# 绘制 Ketogenic 特有通路的 cnetplot
+
 unique_ketogenic_result <- enrich_ketogenic
 unique_ketogenic_result@result <- enrich_ketogenic@result %>% 
   filter(Description %in% unique_ketogenic) %>% 
@@ -194,7 +194,7 @@ cnet_ketogenic <- cnetplot(unique_ketogenic_result, showCategory = 5) +
   ggtitle("Unique to Ketogenic") + 
   theme_minimal()
 
-# 绘制 HFHC 特有通路的 cnetplot
+
 unique_hfhc_result <- enrich_hfhc
 unique_hfhc_result@result <- enrich_hfhc@result %>% 
   filter(Description %in% unique_hfhc) %>% 
@@ -205,7 +205,7 @@ cnet_hfhc <- cnetplot(unique_hfhc_result, showCategory = 5) +
   ggtitle("Unique to HFHC") + 
   theme_minimal()
 
-# 绘制 LFLC 特有通路的 cnetplot
+
 unique_LFLC_result <- enrich_LFLC
 unique_LFLC_result@result <- enrich_LFLC@result %>% 
   filter(Description %in% unique_LFLC) %>% 
@@ -216,10 +216,10 @@ cnet_LFLC <- cnetplot(unique_LFLC_result, showCategory = 5) +
   ggtitle("Unique to LFLC") + 
   theme_minimal()
 
-# 合并图形
+
 # Example of setting explicit dimensions in ggplot2
 combined_cnetplot <- grid.arrange(cnet_common, cnet_high_glucose, cnet_ketogenic, cnet_hfhc, cnet_LFLC, nrow=1)
 
 
-# 保存图像
+
 ggsave(paste0(dirname,"/diet_cnetplot.tiff"), plot = combined_cnetplot, width = 35, height = 8, dpi = 300)
